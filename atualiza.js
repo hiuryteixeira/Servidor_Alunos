00a1c53d108e219d034fd2ef8e5c55a8e1be7a41
hiury <hiuryh2k@gmail.com>
2019-08-23 16:36:35 -0300

    Servidor Rodando

--- atualiza.js
+++ atualiza.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
         {
             document.getElementById("lista").innerHTML += `Nome:
     ${data[i].nome} `
-//             document.getElementById("lista").innerHTML += `Matricula:    
-//     ${data[i].matricula} `
-//             document.getElementById("lista").innerHTML += `Situacao do Curso:
-//     ${data[i].situacao} `
+             document.getElementById("lista").innerHTML += `Matricula:    
+     ${data[i].matricula} `
+             document.getElementById("lista").innerHTML += `Situacao do Curso:
+     ${data[i].situacao} `
             document.getElementById("lista").innerHTML += `Cota:
     ${data[i].cota} <br>` // para filtar e imprimir nome tem que criar vetor e laço de repetição.
     }
--- lista.js
+++ lista.js
@@ -1,7 +1,7 @@
 const url = 'https://dados.ifpb.edu.br/dataset/d02eb6b8-5745-4436-ae22-ef1c182897d9/resource/61f5a0ad-642d-4580-ab62-1110318d0eea/download/alunos.json'
 const axios = require('axios')
 
-const curso = f => f.situacao === 'Conclu\u00eddo' // filtar por contador
+const pessoa = f => f.nome === 'Aline de Almeida Campos' // filtar por contador
 //const scota = f => f.cota === 'null' // O IGUAL E EXATAMENTE IGUAL  FILTRA APOSENTADO
 //const numero = f => f.matricula === '201917520039' // O IGUAL E EXATAMENTE IGUAL  FILTRA APOSENTADO
 
@@ -11,8 +11,8 @@ const curso = f => f.situacao === 'Conclu\u00eddo' // filtar por contador
 axios.get(url).then(resposta => {
     const servidores = resposta.data
     module.exports.resultado =
-    servidores.filter(curso)
+    servidores.filter(pessoa)
 
-    const resultado_filtro = servidores.filter(curso)//.filter(numero)
+    const resultado_filtro = servidores.filter(pessoa)//.filter(numero)
     //console.log(resultado_filtro)
 })
